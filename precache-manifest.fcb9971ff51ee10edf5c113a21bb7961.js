2487a378981b7897d27f48466f99ce19bc49f01e
Elizabeth Le <elizabethlemk@yahoo.com>
2019-05-05 11:30:21 -0400

    Updates

---
+++ precache-manifest.fcb9971ff51ee10edf5c113a21bb7961.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "443180f59860e1f505d7e313875bc1d4",
+    "url": "/my-web-now/index.html"
+  },
+  {
+    "revision": "2241116a3ca3e5aab0cc",
+    "url": "/my-web-now/static/css/2.a56a2503.chunk.css"
+  },
+  {
+    "revision": "69695c00a16960c7376a",
+    "url": "/my-web-now/static/css/main.6c476ea7.chunk.css"
+  },
+  {
+    "revision": "2241116a3ca3e5aab0cc",
+    "url": "/my-web-now/static/js/2.61da16f5.chunk.js"
+  },
+  {
+    "revision": "69695c00a16960c7376a",
+    "url": "/my-web-now/static/js/main.f9a1226f.chunk.js"
+  },
+  {
+    "revision": "c4ae9cdb7cd132a390af",
+    "url": "/my-web-now/static/js/runtime~main.a47d5e1e.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/my-web-now/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/my-web-now/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/my-web-now/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/my-web-now/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/my-web-now/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/my-web-now/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/my-web-now/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/my-web-now/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/my-web-now/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/my-web-now/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/my-web-now/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/my-web-now/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/my-web-now/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/my-web-now/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/my-web-now/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/my-web-now/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
